2a215ad5539e8123b77c63e91447384ec0b9d6eb
unknown <rajudas575@gmail.com>
2025-02-05 21:32:39 +0530

    add srcipt for deploy

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,12 @@ import HomePage from "./pages/HomePage";
 import DetailPage from "./pages/DetailPage";
 
 const App = () => {
-return (
-<>
-<Route path="/" element={} />
-<Route path="/item/:id" element={} />
-</>
-
-);
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/item/:id" element={<DetailPage />} />
+    </Routes>
+  );
 };
 
 export default App;

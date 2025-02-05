8d3b311bedaa334321c0938336d1c378dd123a11
unknown <rajudas575@gmail.com>
2025-02-05 21:56:44 +0530

    add script rout

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 import store from "./redux/store";
-import HomePage from "./pages/HomePage";
-import DetailPage from "./pages/DetailPage";
+import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/item/:id" element={<DetailPage />} />
-      </Routes>
+      <App />
     </Router>
   </Provider>
 );
